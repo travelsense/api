86d5c19ef061df4c434a0c7853b88aaa3f79428f
Alexey Karapetov <karapetov@gmail.com>
2016-03-13 16:04:56 -0700

    Some db tweaks

--- provision/db_setup.sql
+++ provision/db_setup.sql
@@ -1,6 +1,6 @@
 ALTER ROLE postgres PASSWORD 'postgres';
 
 CREATE ROLE vaca_dev LOGIN PASSWORD 'vaca_dev';
-CREATE ROLE vaca_test LOGIN PASSWORD 'vaca_test';
+CREATE ROLE vaca_test LOGIN CREATEDB PASSWORD 'vaca_test';
 
 CREATE DATABASE vaca_dev OWNER=vaca_dev;
