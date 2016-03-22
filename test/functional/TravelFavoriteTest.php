80381dc7b43d92064208d00c35531e5deac1316e
maksymenkool <maksymenkool@gmail.com>
2016-03-22 23:25:35 +0200

    added functional test TravelFavoriteTest

---
+++ test/functional/TravelFavoriteTest.php
@@ -0,0 +1,23 @@
+<?php
+namespace Test;
+
+use Api\Test\FunctionalTestCase;
+
+class TravelFavoriteTest extends FunctionalTestCase
+{
+    public function testAddGetRemoveTravelFavorite()
+    {
+        $this->createAndLoginUser();
+        $id = $this->apiClient->createTravel('Hobbit', 'There and back again');
+        $this->apiClient->addTravelToFavorite($id);
+        $favoriteTravels = $this->apiClient->getAllFavorite();
+        $this->assertEquals(1, count($favoriteTravels));
+        $travel = $favoriteTravels[0];
+//        $response = reset($response);
+        $this->assertEquals('Hobbit', $travel->title);
+        $this->assertEquals('There and back again', $travel->description);
+        $this->apiClient->removeTravelFromFavorite($id);
+        $favoriteTravels = $this->apiClient->getAllFavorite();
+        $this->assertEmpty($favoriteTravels);
+    }
+}
