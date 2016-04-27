a70a7ed6aff0e5eaf57ead59d92bc9345935bb57
RomanVK <roman.kuzmin3333@gmail.com>
2016-04-28 00:51:20 +0300

    added some check

--- test/unit/Security/Authentication/UserAuthenticatorTest.php
+++ test/unit/Security/Authentication/UserAuthenticatorTest.php
@@ -1,21 +1,35 @@
 <?php
 namespace Api\Security\Authentication;
 
+use Api\Security\SessionManager;
 use PHPUnit_Framework_TestCase;
 use Symfony\Component\HttpFoundation\HeaderBag;
 use Symfony\Component\HttpFoundation\Request;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
 
 class UserAuthenticatorTest extends PHPUnit_Framework_TestCase
 {
+    /**
+     * @var Credentials
+     */
     private $credentials;
+    /**
+     * @var SessionManager
+     */
     private $sessionManager;
     /**
      * @var UserAuthenticator
      */
     private $authenticator;
+    /**
+     * @var LoggerInterface
+     */
     private $logger;
+    /**
+     * @var GetResponseEvent
+     */
     private $event;
 
     public function setUp()
@@ -33,6 +47,10 @@ class UserAuthenticatorTest extends PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->setMethods(['getRequest'])
             ->getMock();
+
+        $this->authenticator = new UserAuthenticator($this->credentials, $this->sessionManager, ['excluded']);
+        $this->authenticator->setLogger($this->logger);
+
     }
 
     /**
@@ -42,9 +60,6 @@ class UserAuthenticatorTest extends PHPUnit_Framework_TestCase
      */
     public function testOnRequestThrowsException()
     {
-        $this->authenticator = new UserAuthenticator($this->credentials, $this->sessionManager, ['excluded']);
-        $this->authenticator->setLogger($this->logger);
-
         $request = new Request([], [], ['_route' => 'secured-route']);
         $request->headers = new HeaderBag(['Authorization' => 'Token zzz']);
 
@@ -53,12 +68,17 @@ class UserAuthenticatorTest extends PHPUnit_Framework_TestCase
         $this->authenticator->onRequest($this->event);
     }
 
-    public function testOfRequestThrowsException()
+    public function testOnRequestHappyPath()
     {
-        $this->sessionManager->method('getUserId')->willReturn('zzz');//1
+        $this->sessionManager
+            ->method('getUserId')
+            ->with('zzz')
+            ->willReturn('test_user_id');
 
-        $this->authenticator = new UserAuthenticator($this->credentials, $this->sessionManager, ['excluded']);
-        $this->authenticator->setLogger($this->logger);
+        $this->credentials
+            ->expects($this->once())
+            ->method('setUser')
+            ->with('test_user_id');
 
         $request = new Request([], [], ['_route' => 'secured-route']);
         $request->headers = new HeaderBag(['Authorization' => 'Token zzz']);
@@ -70,11 +90,7 @@ class UserAuthenticatorTest extends PHPUnit_Framework_TestCase
 
     public function testIsExcludedRouteTrue()
     {
-        $this->authenticator = new UserAuthenticator($this->credentials, $this->sessionManager, ['excluded']);
-        $this->authenticator->setLogger($this->logger);
-
         $request = new Request([], [], ['_route' => 'excluded']);//2
-        $request->headers = new HeaderBag(['Authorization' => 'Token zzz']);
 
         $this->event->method('getRequest')->willReturn($request);
 
@@ -83,9 +99,6 @@ class UserAuthenticatorTest extends PHPUnit_Framework_TestCase
 
     public function testNotPregMatchFalse()
     {
-        $this->authenticator = new UserAuthenticator($this->credentials, $this->sessionManager, ['excluded']);
-        $this->authenticator->setLogger($this->logger);
-
         $request = new Request([], [], ['_route' => 'secured-route']);
         $request->headers = new HeaderBag(['Authorization' => 'zzz']);//3
 
@@ -96,6 +109,6 @@ class UserAuthenticatorTest extends PHPUnit_Framework_TestCase
 
     public function testGetSubscribedEvents()
     {
-        $subscribedEvents=UserAuthenticator::getSubscribedEvents();
+        $this->assertEquals(['kernel.request'=>'onRequest'], UserAuthenticator::getSubscribedEvents());
     }
 }
