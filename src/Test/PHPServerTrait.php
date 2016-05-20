f849149842dc43d0dec38455c7411fa805b271e4
Alexey Karapetov <f3ath@users.noreply.github.com>
2016-05-19 23:29:25 -0700

    Starting/stopping webserver only once (#29)

--- src/Test/PHPServerTrait.php
+++ src/Test/PHPServerTrait.php
@@ -9,44 +9,44 @@ trait PHPServerTrait
     /**
      * @var HttpServer
      */
-    private $server;
+    private static $server;
 
     /**
      * @var int
      */
-    protected $port = 8888;
+    protected static $port = 8888;
 
     /**
      * @var float
      */
-    protected $wait = 1.0;
+    protected static $wait = 1.0;
 
     /**
      * @var string
      */
-    protected $host = 'localhost';
+    protected static $host = 'localhost';
 
     /**
      * start the php dev server
      * @param string $log
      */
-    public function startServer(string $log = '/dev/null')
+    public static function startServer(string $log = '/dev/null')
     {
-        if (!$this->server) {
-            $this->server = new HttpServer(
-                $this->host,
-                $this->port,
+        if (!self::$server) {
+            self::$server = new HttpServer(
+                self::$host,
+                self::$port,
                 'app.php',
-                $this->wait
+                self::$wait
             );
         }
-        if ($this->server->isRunning()) {
+        if (self::$server->isRunning()) {
             throw new LogicException('Server is already running');
         }
-        $this->server->setLogFile($log);
+        self::$server->setLogFile($log);
         $dir = getcwd();
         chdir(__DIR__ . '/../../public/');
-        $this->server->start();
+        self::$server->start();
         sleep(1); // Warming up for too long. See Issue #66
         chdir($dir);
     }
@@ -54,10 +54,10 @@ trait PHPServerTrait
     /**
      * stop the php dev server
      */
-    public function stopServer()
+    public static function stopServer()
     {
-        if ($this->server && $this->server->isRunning()) {
-            $this->server->stop();
+        if (self::$server && self::$server->isRunning()) {
+            self::$server->stop();
         } else {
             throw new LogicException('Server is not running');
         }
--- test/functional/FunctionalWebTest.php
+++ test/functional/FunctionalWebTest.php
@@ -4,8 +4,43 @@ namespace Test;
 use Api\Test\ApiClientException;
 use Api\Test\FunctionalTestCase;
 
-class TravelWorkflowTest extends FunctionalTestCase
+class FunctionalWebTest extends FunctionalTestCase
 {
+    public function testUpdateUserDetails()
+    {
+        $this->createAndLoginUser();
+        $user = $this->apiClient->getCurrentUser();
+        $this->assertEquals(
+            (object)[
+                'id'        => 1,
+                'firstName' => 'Alexander',
+                'lastName'  => 'Pushkin',
+                'email'     => 'sasha@pushkin.ru',
+                'picture'   => 'http://pushkin.ru/sasha.jpg',
+            ],
+            $user
+        );
+
+        $this->apiClient->updateUser([
+            'id'        => 1,
+            'firstName' => 'Natalia',
+            'lastName'  => 'Pushkina',
+            'picture'   => 'http://pushkin.ru/sasha.jpg',
+            'email'     => 'sasha@pushkin.ru',
+        ]);
+        $user = $this->apiClient->getCurrentUser();
+        $this->assertEquals(
+            (object)[
+                'id'        => 1,
+                'firstName' => 'Natalia',
+                'lastName'  => 'Pushkina',
+                'email'     => 'sasha@pushkin.ru',
+                'picture'   => 'http://pushkin.ru/sasha.jpg',
+            ],
+            $user
+        );
+    }
+    
     public function testTravelCreationAndRetrieval()
     {
         $this->createAndLoginUser();
