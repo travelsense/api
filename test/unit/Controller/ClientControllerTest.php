2d74a74af24c29165d29106bfa8fe92c582f5e56
shtog97 <shtog97@mail.ru>
2016-06-06 01:05:06 +0300

    Update ClientControllerTest.php

--- test/unit/Controller/ClientControllerTest.php
+++ test/unit/Controller/ClientControllerTest.php
@@ -5,16 +5,15 @@ use Api\Controller\ClientController;
 
 class ClientControllerTest extends \PHPUnit_Framework_TestCase
 {
-    /** @test **/
     public function testClient()
     {
         $controller = new ClientController();
         $this->assertEquals(
-            ([
+            [
                 'version' => "0.0.0",
                 'supported' => true,
-            ]),
+            ],
             $controller->version("0.0.0")
-            );
+        );
     }
 }
