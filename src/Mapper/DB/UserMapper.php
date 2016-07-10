6bb62985fb279853bfc86ca2816b3e7ec06da764
shtog97 <shtog97@mail.ru>
2016-07-11 00:32:27 +0300

    Update UserMapper.php

--- src/Mapper/DB/UserMapper.php
+++ src/Mapper/DB/UserMapper.php
@@ -58,12 +58,12 @@ VALUES
 RETURNING id, created
 SQL;
         $insert = $this->pdo->prepare($sql);
-	$insert->bindValue('email', $user->getEmail(), PDO::PARAM_STR);
-	$insert->bindValue('password', $this->getPasswordHash($user->getPassword()), PDO::PARAM_STR);
-	$insert->bindValue('first_name', $user->getFirstName(), PDO::PARAM_STR);
-	$insert->bindValue('last_name', $user->getLastName(), PDO::PARAM_STR);
-	$insert->bindValue('picture', $user->getPicture(), PDO::PARAM_STR);
-	$insert->bindValue('creator', $user->getCreator(), PDO::PARAM_BOOL);
+        $insert->bindValue('email', $user->getEmail(), PDO::PARAM_STR);
+        $insert->bindValue('password', $this->getPasswordHash($user->getPassword()), PDO::PARAM_STR);
+        $insert->bindValue('first_name', $user->getFirstName(), PDO::PARAM_STR);
+        $insert->bindValue('last_name', $user->getLastName(), PDO::PARAM_STR);
+        $insert->bindValue('picture', $user->getPicture(), PDO::PARAM_STR);
+        $insert->bindValue('creator', $user->getCreator(), PDO::PARAM_BOOL);
        
 	$row = $insert->fetch(PDO::FETCH_ASSOC);
         $user->setId($row['id']);
