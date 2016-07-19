1aa6295d2fa55025de41f15b6152c9e90f576c2d
Alexey Karapetov <karapetov@gmail.com>
2016-07-18 19:44:51 -0700

    migrator wip

--- src/Test/DatabaseTrait.php
+++ src/Test/DatabaseTrait.php
@@ -2,6 +2,7 @@
 namespace Api\Test;
 
 use Api\Application;
+use Migrator\Migrator;
 use PDO;
 
 trait DatabaseTrait
@@ -18,8 +19,11 @@ trait DatabaseTrait
             $pdo->exec("SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '{$db['database']}'");
             $pdo->exec("DROP DATABASE IF EXISTS {$db['database']}");
             $pdo->exec("CREATE DATABASE {$db['database']} OWNER={$db['user']}");
-            $app["db.$name.pdo"]->exec(file_get_contents(__DIR__ . '/../../db/ext/postgis.sql'));
-            $app["db.$name.migrator"]->upgrade();
+            $app["db.$name.pdo"]->exec(file_get_contents(__DIR__ . "/../../db/{$db}/ext/postgis.sql"));
+            /** @var Migrator $migrator */
+            $migrator = $app["db.migrator.factory"]->getMigrator($name);
+            list(,, $highest) = $migrator->getVersionRange();
+            $migrator->migrateTo($highest);
         }
     }
 }
