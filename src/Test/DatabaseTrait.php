a5e49ddb6e7cdcfe4dd8189f608a6cfdc2281d76
Alexey Karapetov <karapetov@gmail.com>
2016-08-03 20:45:47 -0700

    typo

--- src/Test/DatabaseTrait.php
+++ src/Test/DatabaseTrait.php
@@ -19,7 +19,7 @@ trait DatabaseTrait
             $pdo->exec("SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '{$db['database']}'");
             $pdo->exec("DROP DATABASE IF EXISTS {$db['database']}");
             $pdo->exec("CREATE DATABASE {$db['database']} OWNER={$db['user']}");
-            $app["db.$name.pdo"]->exec(file_get_contents(__DIR__ . "/../../db/{$db}/ext/postgis.sql"));
+            $app["db.$name.pdo"]->exec(file_get_contents(__DIR__ . "/../../db/{$name}/ext/postgis.sql"));
             /** @var Migrator $migrator */
             $migrator = $app["db.migrator.factory"]->getMigrator($name);
             list(,, $highest) = $migrator->getVersionRange();
