3abd3fd4cae00e75e023773534fe7e7f1f9363b6
maksymenkool <maksymenkool@gmail.com>
2016-08-13 11:39:03 +0300

    rename to is_favorited

--- src/Controller/Travel/TravelController.php
+++ src/Controller/Travel/TravelController.php
@@ -304,11 +304,11 @@ class TravelController extends ApiController
 
     /**
      * @param Travel $travel
-     * @param bool   $is_favorite
+     * @param bool   $is_favorited
      * @param bool   $minimized
      * @return array
      */
-    private function buildTravelView(Travel $travel, bool $is_favorite, bool $minimized = false): array
+    private function buildTravelView(Travel $travel, bool $is_favorited, bool $minimized = false): array
     {
         $view = [];
         $view['id'] = $travel->getId();
@@ -333,7 +333,7 @@ class TravelController extends ApiController
                 ];
             }
         }
-        $view['is_favorited'] = $is_favorite;
+        $view['is_favorited'] = $is_favorited;
         if (count($travel->getContent())) { // TODO Refactor this logic. Move to Travel?
             $travel->setActions($this->createActions($travel->getContent(), $travel->getId()));
         }
