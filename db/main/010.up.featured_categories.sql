89fa2473a113d806a677ecec8451361773be996b
maksymenkool <maksymenkool@gmail.com>
2016-08-21 09:57:02 +0300

    add value featured = true

--- db/main/010.up.featured_categories.sql
+++ db/main/010.up.featured_categories.sql
@@ -1,1 +1,5 @@
 ALTER TABLE categories ADD COLUMN featured BOOLEAN NOT NULL DEFAULT FALSE;
+
+UPDATE categories SET featured = true WHERE name = 'Featured';
+UPDATE categories SET featured = true WHERE name = 'Romantic';
+UPDATE categories SET featured = true WHERE name = 'Sports';
