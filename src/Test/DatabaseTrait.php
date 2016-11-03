56590f69e50179d90df5d6e51e4a1ea973575afb
Alexey Karapetov <karapetov@gmail.com>
2016-11-03 14:23:46 -0700

    Style

--- src/Test/DatabaseTrait.php
+++ src/Test/DatabaseTrait.php
@@ -16,7 +16,11 @@ trait DatabaseTrait
                 $db['password'],
                 [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
             );
-            $admin_pdo->exec("SELECT pg_terminate_backend(pid) FROM pg_stat_activity WHERE datname = '{$db['database']}'");
+            $admin_pdo->exec("
+              SELECT pg_terminate_backend(pid) 
+              FROM pg_stat_activity 
+              WHERE datname = '{$db['database']}'
+            ");
             $admin_pdo->exec("DROP DATABASE IF EXISTS {$db['database']}");
             $admin_pdo->exec("CREATE DATABASE {$db['database']} OWNER={$db['user']}");
             $app['dbs'][$name]->exec(file_get_contents(__DIR__ . "/../../db/{$name}/ext/postgis.sql"));
