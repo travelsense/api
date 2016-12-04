637fb3c9a74c9817e3bc8107dd2efc1386d1b12c
Alexey Karapetov <karapetov@gmail.com>
2016-12-03 22:05:16 -0800

    drop travels.content

---
+++ db/main/Version20161204055508.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Api\DB\Migration;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Remove travel.content
+ */
+class Version20161204055508 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        $this->addSql('ALTER TABLE travels DROP COLUMN content;');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        $this->throwIrreversibleMigrationException('Dropped columns can not be reverted');
+    }
+}
