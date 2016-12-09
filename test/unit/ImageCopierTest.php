df675f8810f008cf1b41d35db85cedc13a04e3cd
maksymenkool <maksymenkool@gmail.com>
2016-12-09 15:29:32 +0200

    added test err

--- test/unit/ImageCopierTest.php
+++ test/unit/ImageCopierTest.php
@@ -3,9 +3,8 @@ namespace Api;
 
 use Api\Service\ImageCopier;
 use Api\Service\ImageLoader;
-use phpDocumentor\Reflection\Types\Resource;
+use Exception;
 use PHPUnit_Framework_TestCase;
-use Symfony\Component\HttpFoundation\Request;
 
 class ImageCopierTest extends PHPUnit_Framework_TestCase
 {
@@ -30,7 +29,6 @@ class ImageCopierTest extends PHPUnit_Framework_TestCase
             ->method('upload')
             ->with($this->callback(function ($value) {
                 if (getType($value)==='resource') {
-                    var_dump($value);
                     return $value;
                 }
                 return false;
@@ -45,5 +43,31 @@ class ImageCopierTest extends PHPUnit_Framework_TestCase
             'https://static.hoptrip.us/36/43/36439437709f38e3800e7d08504626b170d651d5',
             $link
         );
+    }
+
+    public function testCopyFromError()
+    {
+        $this->image_loader = $this->getMockBuilder(ImageLoader::class)
+            ->setMethods(['upload'])
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $this->image_copier = $this->getMockBuilder(ImageCopier::class)
+            ->setMethods(['copyFrom'])
+            ->setConstructorArgs([$this->image_loader, 5])
+            ->getMock();
+
+        $fake_remote_uri = '/example.com/my-avatar.jpg';
+
+        $this->image_copier->expects($this->once())
+            ->method('copyFrom')
+            ->willThrowException(new Exception("Could not open the file $fake_remote_uri"));
+        
+        try {
+            $this->image_copier->copyFrom($fake_remote_uri);
+        } catch (Exception $e) {
+            var_dump($e->getMessage());
+            $this->assertEquals("Could not open the file $fake_remote_uri", $e->getMessage());
+        }
     }
 }
