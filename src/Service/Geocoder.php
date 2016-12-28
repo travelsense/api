8d181282ec50cf81c80919e15223b7f09eb2c021
maksymenkool <maksymenkool@gmail.com>
2016-12-28 18:17:51 +0200

    added funcs filePutDate, getTravelsUpdatedAfter

--- src/Service/Geocoder.php
+++ src/Service/Geocoder.php
@@ -2,6 +2,7 @@
 namespace Api\Service;
 
 use Api\Mapper\DB\TravelMapper;
+use DateTime;
 use GoogleMapsGeocoder;
 
 class Geocoder
@@ -16,6 +17,8 @@ class Geocoder
      */
     private $travel_mapper;
 
+    private $file_name = '/tmp/updated/last_updated.txt';
+
     /**
      * Geocoder constructor.
      * @param GoogleMapsGeocoder $geocoder
@@ -25,5 +28,22 @@ class Geocoder
     {
         $this->geocoder = $geocoder;
         $this->travel_mapper = $travel_mapper;
+    }
+
+    public function setCitiesStatesCountriesToTravel()
+    {
+
+    }
+
+    private function getTravelsUpdatedAfter(DateTime $date_time)
+    {
+        $travels = $this->travel_mapper->fetchUpdatedAfter($date_time);
+        return $travels;
+    }
+
+    private function filePutDate($date_time)
+    {
+        $file = file($this->file_name);
+        file_put_contents($file, $date_time);
     }
 }
