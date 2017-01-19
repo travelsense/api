e43cc7eb36c9b457a4280342cfbd7b6026d9e127
Alexey <f3ath@users.noreply.github.com>
2017-01-18 22:48:47 -0800

    Update MailerTest.php

--- test/integration/MailerTest.php
+++ test/integration/MailerTest.php
@@ -66,7 +66,7 @@ class MailerTest extends PHPUnit_Framework_TestCase
         $test = $this;
         $this->mailer->method('send')->willReturnCallback(function (Swift_Message $msg) use ($test) {
             $test->assertEquals(
-            "    Hop Trip Statistic - ".date('F d, Y')."\n\n    Users: 2 (-1)\n    Travels: 10 (+5)\n",
+            "    Hop Trip Stats - ".date('F d, Y')."\n\n    Users: 2 (-1)\n    Travels: 10 (+5)\n",
                 $msg->getBody()
             );
             $test->assertEquals(
